eab25a0bb40bbc7ffeeaf6293563f30c908c4bb6
elvisbui <elvisbui.ed@gmail.com>
2019-05-09 01:00:23 -0400

    Init Branch & README edits
    Init Android Project
    
    
    Initial Timer Base

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # PomodoroTime
 A tomato timer app based on the Pomodoro Technique created to as a project to learn more Android Development.
+
+>Author: Elvis Bui
+
+I am creating this app to improve my skills regarding Android development and to provide myself with a timer
+to help my time management. This timer is based off of Francesco Cirillo's "Pomodoro Technique" that begins
+with a 25 minute timer (a pomodoro) followed by a 5 minute short break. After four pomodoros, a long break
+of 15 minutes is given and the cycle repeats.
+
+Find out more about the Pomodoro Technique [here](https://en.wikipedia.org/wiki/Pomodoro_Technique).
+
+## Special Thanks To:
+* Francesco Cirillo for creating the Pomodoro Technique.
+* Martina Smith for introducing the Pomodoro Technique to me.
+* Krzysztof J. Kochut for teaching me the fundamentals of Android Development at the University of Georgia.
+* Sarah L. Smith for being the love of my love, recommending me to create this app, and providing useful user stories.
+
+## Contact Me
+* elvisbui.cs@gmail.com
---
+++ app/src/main/java/dev/elvisbui/pomodorotime/MainActivity.java
@@ -0,0 +1,100 @@
+package dev.elvisbui.pomodorotime;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 6000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        mTextViewCountDown = findViewById(R.id.countdown);
+        mButtonStartPause = findViewById(R.id.startPauseButton);
+        mButtonReset = findViewById(R.id.resetButton);
+        
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mTimerRunning){
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+        
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+
+        updateCountDownText();
+    }
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Resume");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setText("Start");
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+}
